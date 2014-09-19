8b7a1024818a60f2657a4feafeb663bc1910e5f1
ginjal <ginjal@users.noreply.github.com>
2014-09-20 00:30:38 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,11 @@
+// Gfycat Helper - Chrome Extention
+// Copyright (C) 2014  Stefan Vosloo
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+
 var arr = document.getElementsByTagName('a');
 for(var i=0;i<arr.length;i++){
     var a = arr[i]
